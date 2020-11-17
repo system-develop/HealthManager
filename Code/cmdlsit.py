745e35861cd268b0b0db4b51b7e0040a71397ea0
maru8375 <B8375@oic.jp>
2020-11-17 14:58:46 +0900

    cmdlsit
    これやり方ちゃんとあってるかわからんけどとりあえずcmdlist

---
+++ Code/cmdlsit.py
@@ -0,0 +1,36 @@
+# インストールした discord.py を読み込む
+import discord
+
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = ''
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+# サーバー加入時の自動メッセージ
+#@client.event
+#async def on_guild_join():
+ #   channel = client.get_channel(12324234183172)
+  #  await channel.send('!cmdlistでコマンドリストが見れます。')
+
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「!cmdlist」と発言したら「対応するコマンド」が返る処理
+    if message.content == '!cmdlist':
+        await message.channel.send('``` !health_対応する絵文字 → 現在の体調を絵文字で表す。\
+        \n !tmep_〇〇.〇 → 現在の体温を記録する。\
+        \n !elist → !helthの対応する絵文字を表示する。\
+        \n !mylist → 自分が投稿した過去の情報を返す。```')
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
