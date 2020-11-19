28e4e89e7b639161071d8e5aab269270287aeed9
maru8375 <B8375@oic.jp>
2020-11-19 16:59:10 +0900

    Update cmdlsit.py

--- Code/cmdlsit.py
+++ Code/cmdlsit.py
@@ -7,6 +7,9 @@ TOKEN = ''
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
+#BOTのサーバー加入  時に流すメッセージ用
+gjoin = discord.on_guild_join()
+
 # 起動時に動作する処理
 @client.event
 async def on_ready():
@@ -27,9 +30,10 @@ async def on_message(message):
         return
     # 「!cmdlist」と発言したら「対応するコマンド」が返る処理
     if message.content == '!cmdlist':
-        await message.channel.send('``` !health_対応する絵文字 → 現在の体調を絵文字で表す。\
-        \n !tmep_〇〇.〇 → 現在の体温を記録する。\
-        \n !elist → !helthの対応する絵文字を表示する。\
+        await message.channel.send\
+        ('``` !health_対応する絵文字 → 現在の体調を絵文字で表す。\
+        \n !temp_〇〇.〇 → 現在の体温を記録する。\
+        \n !elist → !healthの対応する絵文字を表示する。\
         \n !mylist → 自分が投稿した過去の情報を返す。```')
 
 # Botの起動とDiscordサーバーへの接続
